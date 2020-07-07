fa07381fac249154901faaba421248c9b29939b7
IceMistDragon <43175064+IceMistDragon@users.noreply.github.com>
2020-07-07 20:21:39 +0800

    200707

--- Ghost-Hostel/Assets/Rule/GAMEOVER.cs
+++ Ghost-Hostel/Assets/Rule/GAMEOVER.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 
 //主角摸到出口門  遊戲結束
 
 public class GAMEOVER : MonoBehaviour
 {
-    
+    public FirstPersonController fpc;
+    public GameObject final;
+
+    private void Start()
+    {
+        Cursor.visible = false;                           //指標隱藏
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "Export")
+        {
+            final.SetActive(true);
+            fpc.enabled = false;
+            Cursor.visible = true;
+            
+            
+        }
+    }
 }
---
+++ Ghost-Hostel/Assets/Rule/TableOfContents.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TableOfContents : MonoBehaviour
+{
+    // 方法
+    // public void 名稱 () {  }
+
+    public void CharacterBook()
+    {
+        SceneManager.LoadScene("CharacterBook");
+    }
+}
